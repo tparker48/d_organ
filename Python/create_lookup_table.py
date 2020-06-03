a4769266ff1692c5ea7bb57b7a4b8eb577be7871
Thomas Parker <32755663+tparker48@users.noreply.github.com>
2020-06-02 23:09:49 -0700

    blah

---
+++ Python/create_lookup_table.py
@@ -0,0 +1,16 @@
+file = open("Linear to Logarithmic Lookup Table.txt",'w')
+file.write('\\\\ This auto-generated file provides a lookup table for converting the linear potentiometer readings to a logarithmic scale\n')
+file.write('int lin2log[1024] = {\n')
+
+comma = ','
+
+lookup = []
+for i in range(1024):
+    lookup.append(.25*(5**(i/1023.0))-.25)
+    print(lookup[i])
+    if(i==1023):
+        comma = ''
+    file.write(str(lookup[i]) + comma)
+
+file.write('};')
+file.close()
